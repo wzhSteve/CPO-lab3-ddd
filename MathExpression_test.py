4920884fabc70aadd27e69d9b8eead4d7675a28a
WzhRslh <38376151+wzhSteve@users.noreply.github.com>
2022-06-19 14:42:37 +0800

    Update MathExpression_test.py

--- MathExpression_test.py
+++ MathExpression_test.py
@@ -27,23 +27,21 @@ class TestMathExpression(unittest.TestCase):
         self.assertEqual(2*6-sin(1)*(1-3), out2)
         # test3
         str = "a + 2 - f(b, c)*(b - c)"
-        func_dic = lambda a, b: sin(a) + b
-        test3 = expression(str=str, func_dic=func_dic, a=2, b=1, c=3)
+        test3 = expression(str=str, func_dic=lambda a, b: sin(a) + b,
+                           a=2, b=1, c=3)
         value3 = test3.process()
         out3 = direct_cal(value3)
         self.assertEqual(2 + 2 - (sin(1) + 3) * (1 - 3), out3)
         # test4
         str = "a + 2 - f(b,c,d,e)*(b - c)"
-        func_dic = lambda a, b, c, d: sin(a) + b * c - d
-        test4 = expression(str=str, func_dic=func_dic,
+        test4 = expression(str=str, func_dic=lambda a, b, c, d: sin(a)+b*c-d,
                            a=2, b=1, c=3, d=4, e=8)
         value4 = test4.process()
         out4 = direct_cal(value4)
         self.assertEqual(2 + 2 - (sin(1) + 3*4-8) * (1 - 3), out4)
         # test5
         str = "a + 2 - f(b,c,d,e)*(b - c)"
-        func_dic = lambda a, b, c, d: sin(a) * b * (c - d)
-        test5 = expression(str=str, func_dic=func_dic,
+        test5 = expression(str=str, func_dic=lambda a, b, c, d: sin(a)*b*(c-d),
                            a=2, b=1, c=3, d=4, e=8)
         value5 = test5.process()
         out5 = direct_cal(value5)
